cbf25ba32c2277191612b1fb71e50a73393154a7
Carlos Eduardo Silva Barbosa <carlos.institucional@outlook.com>
2023-06-28 19:58:21 -0300

    first commit

---
+++ 1.js
@@ -0,0 +1,16 @@
+// 1. Considere o seguinte array:
+//const salarios = [5000.00, 5460.50, 3452.00, 6900.00, 7590.10, 8012.99, 1290.00, 15000.00];
+// a. Imprima no console o índice do primeiro salário maior que
+// 7.500 utilizando o findIndex
+// b. Crie uma nova lista filtrada com os salários que são maior que
+// 8.000 utilizando o filter
+
+
+
+
+const salarios = [5000.00, 5460.50, 3452.00, 6900.00, 7590.10, 8012.99, 1290.00, 15000.00];
+
+const indiceSalarioMaior7500 = salarios.findIndex(salario => salario > 7500);
+console.log("Índice do primeiro salário maior que 7.500:", indiceSalarioMaior7500);
+const salariosMaiores8000 = salarios.filter(salario => salario > 8000);
+console.log("Salários maiores que 8.000:", salariosMaiores8000);
